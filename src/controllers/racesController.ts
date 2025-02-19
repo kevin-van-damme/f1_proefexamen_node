220f94184e31002d7c63f26e74212b1db7590380
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-02-19 12:55:12 +0100

    final ommit

--- src/controllers/racesController.ts
+++ src/controllers/racesController.ts
@@ -1,12 +1,35 @@
 import { Request, Response } from "express";
 import { Error as MongooseError } from "mongoose";
 import { Race } from "../models/raceModel";
+import { Driver } from "../models/driverModel";
 const { ValidationError } = MongooseError;
 
 export const getRaces = async (req: Request, res: Response) => {
   try {
     const races = await Race.find();
-    res.status(200).json(races);
+    const drivers = await Driver.find();
+    if (!races || races.length === 0) {
+      return res.status(404).json({ message: "No races found" });
+    }
+    const racesWithDetails = drivers.map((driver) => {
+      const countryFlag = `https://purecatamphetamine.github.io/country-flag-icons/3x2/${race.countryCode}.svg`;
+      console.log(racesWithDetails, countryFlag);
+      races.race_results = races.race_results.map((result) => {
+        const driver = drivers.find(
+          (driver) => driver.driver_id === result.driver_id
+        );
+        return {
+          ...result,
+          driver: driver ? driver : null,
+        };
+      });
+      return {
+        ...races,
+        countryFlag,
+      };
+    });
+
+    res.status(200).json(racesWithDetails);
   } catch (error: unknown) {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
--- src/models/raceModel.ts
+++ src/models/raceModel.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Types } from "mongoose";
 
 const raceSchema = new mongoose.Schema({
   round: {
